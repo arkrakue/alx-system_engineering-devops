88f758d702a61bedfc2796933b0ddb47871c518a
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-10-23 14:45:25 +0000

    handling failed requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# handling more HTTP rquests
+
+exec { 'create-ab-test-script':
+  command => "/bin/bash -c 'echo -e \"#!/bin/bash\nab -c 100 -n 2000 -l localhost/\" \
+               > /usr/local/bin/ab-test && chmod 0755 /usr/local/bin/ab-test'",
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  unless  => 'test -f /usr/local/bin/ab-test',
+}
