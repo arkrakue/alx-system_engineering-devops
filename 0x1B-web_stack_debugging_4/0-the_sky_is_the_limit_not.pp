8053549bd86a8f2ae74fbb50a1357140214d8140
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-10-24 22:18:27 +0000

    Stracing

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # handling more HTTP rquests
 exec { 'create-ab-test-script':
-  command => "/bin/bash -c 'echo -e \"#!/bin/bash\nab -c 100 -n 2000 -l localhost/\" \
+  command => "/bin/bash -c 'echo -e \"#!/bin/bash\nab -c 100 -n 1000 -l localhost/\" \
                > /usr/local/bin/ab-test && chmod 0755 /usr/local/bin/ab-test'",
   path    => ['/bin', '/usr/bin', '/usr/sbin'],
   unless  => 'test -f /usr/local/bin/ab-test',
